63fe901114dcb11cb3800d9653f99aba8087cb06
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-10-02 00:59:21 +0100

    Add clear button

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ if(!isset($_SESSION['id'])) {
         <h1>Welcome to note creator!</h1>
         <div class="container">
             <div class="column">
-                <h2>Your HTML code <input type="file" onchange="loadFile(this.files[0])"><button id="saveButton" disabled onclick="save()">Save As</button><button id="shareButton" disabled onclick="share()">Share</button></h2>
+                <h2>Your HTML code <input type="file" onchange="loadFile(this.files[0])"><button id="saveButton" disabled onclick="save()">Save As</button><button id="shareButton" disabled onclick="share()">Share</button><button id="clearButton" disabled onclick="remove()">Clear</button></h2>
                 <textarea id="textarea"></textarea>
             </div>
             <div class="column">
@@ -76,6 +76,7 @@ if(!isset($_SESSION['id'])) {
             function disableMenu(value) {
 		saveButton.disabled = value;
 		shareButton.disabled = value;
+		clearButton.disabled = value;
             }
 
             let pastContent = localStorage.getItem("html");
@@ -114,6 +115,11 @@ if(!isset($_SESSION['id'])) {
 
             function share() {
                 navigator.share({text: textarea.value})
+	    }
+
+	    function remove() {
+                textarea.value = "";
+		disableMenu(true);
 	    }
         </script>
     </body>
