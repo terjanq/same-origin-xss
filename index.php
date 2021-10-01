9af6b53360f46e90c5385cd9f10f23c3bdb4325c
terjanq <terjanq@users.noreply.github.com>
2021-10-01 23:58:52 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+isset($_GET['source']) && highlight_file(__FILE__) && die();
+
+header('X-Frame-Options: SAMEORIGIN');
+
+session_set_cookie_params(60, '/; samesite=Lax', $_SERVER['HTTP_HOST'], true, true);
+session_start();
+
+if(!isset($_SESSION['id'])) {
+    $identifier = bin2hex(random_bytes(12));
+    $_SESSION['id'] = $identifier;
+}else{
+    $identifier = $_SESSION['id'];
+}
+
+?>
+<html>
+    <head>
+        <script>
+            const identifier = '<?=$identifier;?>';
+            onmessage = e => {
+                const data = e.data; 
+                if(e.origin !== window.origin && data.identifier !== identifier) return;
+                if(data.type === 'render'){
+                    renderContainer.innerHTML = data.body;
+                }
+            }
+         </script>
+    </head>
+    <body>
+            <div id="renderContainer"></div>
+    </body>
+</html>
