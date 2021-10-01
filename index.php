dcd7158f4c5662a7cdb4f4cf5327ee0e8b7cb3de
terjanq <terjanq@users.noreply.github.com>
2021-10-01 23:59:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 isset($_GET['source']) && highlight_file(__FILE__) && die();
-
 header('X-Frame-Options: SAMEORIGIN');
 
 session_set_cookie_params(60, '/; samesite=Lax', $_SERVER['HTTP_HOST'], true, true);
@@ -19,16 +18,61 @@ if(!isset($_SESSION['id'])) {
     <head>
         <script>
             const identifier = '<?=$identifier;?>';
-            onmessage = e => {
-                const data = e.data; 
-                if(e.origin !== window.origin && data.identifier !== identifier) return;
-                if(data.type === 'render'){
-                    renderContainer.innerHTML = data.body;
-                }
-            }
          </script>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.3.3/purify.min.js" integrity="sha512-gtcrasYnyeB27+IejClswFlb/eggt+khRr+lLAeNcgg5x2ijlWaiBOPXZkwivNj15LaE6s9CzV57hsoTPrQ5xg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+         <style>
+             .container {
+                 width: 90vw;
+                 height: 80vh;
+                 padding: 0;
+                 margin: 0;
+                 display: inline-block;
+             }
+             .container .column{
+                 width: 30vw;
+                 float: left;
+             }
+             .container textarea{
+                 width: 100%;
+                 height: 100%;
+             }
+             .container iframe{
+                 width: 100%;
+                 height: 100%;
+             }
+         </style>
     </head>
     <body>
-            <div id="renderContainer"></div>
+        <h1>Welcome to note creator!</h1>
+        <div class="container">
+            <div class="column">
+                <h2>Your HTML code</h2>
+                <textarea id="textarea"></textarea>
+            </div>
+            <div class="column">
+                <h2>Rendered page</h2>
+                <iframe id="iframe" src="/iframe.php"></iframe>
+            </div>
+            <div class="column">
+                <h2>Hall of Fame</h2>
+                <ul>
+                    <li>
+                        You?
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+        <script>
+            textarea.onchange = textarea.oninput = () => {
+                const dirty = textarea.value;
+                const clean = DOMPurify.sanitize(dirty);
+                iframe.contentWindow.postMessage({
+                    identifier,
+                    type: 'render',
+                    body: clean,
+                }, window.origin);
+            }
+        </script>
     </body>
 </html>
