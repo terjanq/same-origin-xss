4073156db3820e9883ed1c4ca9000a504586f6d9
terjanq <terjanq@gmail.com>
2021-10-07 21:50:46 +0100

    remove test data

--- private/hof.php
+++ private/hof.php
@@ -8,9 +8,12 @@ for($i=0; $i<count($csv); $i++){
     $user = htmlentities($data[0]);
     $handle = htmlentities($data[1], ENT_QUOTES);
     $comment = htmlentities($data[2]);
-    if(strlen(trim($comment)) != 0) $comment = "<span class=\"comment\">$comment</span>";
-    else $comment = '';
-    $hof.="<li><a href=\"https://twitter.com/$handle\">$user</a> $comment</li>\n";
+    if(strlen(trim($comment)) == 0) $comment = '';
+    else $comment = "<span class=\"comment\">$comment</span>";
+
+    if(strlen(trim($handle)) == 0) $handle = '';
+    else $handle = "href=\"https://twitter.com/$handle\"";
+    $hof.="<li><a $handle>$user</a> $comment</li>\n";
 }
 
 ?>
